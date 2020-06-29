3e6b572b4157a3064943db4d19770d5139788985
Joe Biggins <josephjbiggins@icloud.com>
2020-06-29 15:02:30 -0500

    Completed Movie rental system

---
+++ cmake-build-debug/CMakeFiles/hw1_support.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1_support.dir/helperfunctions.cpp.o"
+  "libhw1_support.a"
+  "libhw1_support.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw1_support.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
